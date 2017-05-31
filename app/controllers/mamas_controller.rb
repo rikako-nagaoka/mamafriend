b10c05e6fe816aa58083ab1050f90382a1ec06ae
永岡里果子 <rikako2943@gmail.com>
2017-05-31 17:57:35 +0900

    Index index

---
+++ app/controllers/mamas_controller.rb
@@ -0,0 +1,2 @@
+class MamasController < ApplicationController
+end
---
+++ test/controllers/mamas_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MamasControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
