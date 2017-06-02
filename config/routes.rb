ee978a0856e1c7eed68a0825c25047017c2262ff
永岡里果子 <rikako2943@gmail.com>
2017-06-02 18:23:18 +0900

    マイページを表示する

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: :index
+  resources :users, only: [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
